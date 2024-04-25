6d32723977ea3b98c3e351593030c185cc8a0e16
unknown <dingx92@gmail.com>
2024-04-25 23:01:13 +0800

    add files

--- RC-49/RC-49_64x64/class-conditional/StudioGAN/scripts/run_train1.bat
+++ RC-49/RC-49_64x64/class-conditional/StudioGAN/scripts/run_train1.bat
@@ -4,8 +4,8 @@
 
 @echo off
 
-set ROOT_PREFIX=D:/BaiduSyncdisk/Baidu_WD/CCGM/CcDDPM/RC-49/RC-49_64x64
-set DATA_PATH=D:/BaiduSyncdisk/Baidu_WD/datasets/CCGM_or_regression/RC-49
+set ROOT_PREFIX=<YOUR_PATH>/CCDM/RC-49/RC-49_64x64
+set DATA_PATH=<YOUR_PATH>/CCDM/datasets/RC-49
 set EVAL_PATH=%ROOT_PREFIX%/evaluation
 set CKPT_EVAL_FID_PATH=%EVAL_PATH%/ckpt_AE_epoch_200_seed_2020_CVMode_False.pth
 set CKPT_EVAL_LS_PATH=%EVAL_PATH%/ckpt_PreCNNForEvalGANs_ResNet34_regre_epoch_200_seed_2020_CVMode_False.pth
@@ -17,18 +17,20 @@ set CONFIG_PATH=./configs/%GAN_NAME%.yaml
 wandb disabled
 @REM wandb online
 
-set CUDA_VISIBLE_DEVICES=0
-
-@REM python main.py --train -metrics none ^
-@REM     --data_dir %DATA_PATH% --cfg_file %CONFIG_PATH% --save_dir ./output/%GAN_NAME% ^
-@REM     --seed 2023 --num_workers 8 ^
-@REM     --print_freq 1000 --save_freq 5000 ^ %*
 
 
+@REM for training
+python main.py --train -metrics none ^
+    --data_dir %DATA_PATH% --cfg_file %CONFIG_PATH% --save_dir ./output/%GAN_NAME% ^
+    --seed 2023 --num_workers 8 ^
+    --print_freq 1000 --save_freq 5000 ^ %*
+
+
+
+@REM for evaluation
 set CKPT_G=./output/%GAN_NAME%/checkpoints/RC49-ReACGAN-train-2024_01_05_23_23_57/model=G-current-weights-step=30000.pth
 set CKPT_G_EMA=./output/%GAN_NAME%/checkpoints/RC49-ReACGAN-train-2024_01_05_23_23_57/model=G_ema-current-weights-step=30000.pth
 set dump_niqe_path=D:/LocalWD/CcGAN_TPAMI_NIQE/RC-49/NIQE_64x64/fake_data
-
 python main.py -metrics none ^
     --data_dir %DATA_PATH% --cfg_file %CONFIG_PATH% --save_dir ./output/%GAN_NAME% ^
     --seed 2023 --num_workers 8 ^
--- RC-49/RC-49_64x64/class-conditional/StudioGAN/scripts/run_train2.bat
+++ RC-49/RC-49_64x64/class-conditional/StudioGAN/scripts/run_train2.bat
@@ -4,8 +4,8 @@
 
 @echo off
 
-set ROOT_PREFIX=D:/BaiduSyncdisk/Baidu_WD/CCGM/CcDDPM/RC-49/RC-49_64x64
-set DATA_PATH=D:/BaiduSyncdisk/Baidu_WD/datasets/CCGM_or_regression/RC-49
+set ROOT_PREFIX=<YOUR_PATH>/CCDM/RC-49/RC-49_64x64
+set DATA_PATH=<YOUR_PATH>/CCDM/datasets/RC-49
 set EVAL_PATH=%ROOT_PREFIX%/evaluation
 set CKPT_EVAL_FID_PATH=%EVAL_PATH%/ckpt_AE_epoch_200_seed_2020_CVMode_False.pth
 set CKPT_EVAL_LS_PATH=%EVAL_PATH%/ckpt_PreCNNForEvalGANs_ResNet34_regre_epoch_200_seed_2020_CVMode_False.pth
@@ -17,19 +17,21 @@ set CONFIG_PATH=./configs/%GAN_NAME%.yaml
 wandb disabled
 @REM wandb online
 
-set CUDA_VISIBLE_DEVICES=0
-
-@REM python main.py --train -metrics none ^
-@REM     --data_dir %DATA_PATH% --cfg_file %CONFIG_PATH% --save_dir ./output/%GAN_NAME% ^
-@REM     --seed 2023 --num_workers 8 ^
-@REM     -sync_bn -mpc ^
-@REM     --print_freq 1000 --save_freq 5000 ^ %*
 
 
+@REM for training
+python main.py --train -metrics none ^
+    --data_dir %DATA_PATH% --cfg_file %CONFIG_PATH% --save_dir ./output/%GAN_NAME% ^
+    --seed 2023 --num_workers 8 ^
+    -sync_bn -mpc ^
+    --print_freq 1000 --save_freq 5000 ^ %*
+
+
+
+@REM for evaluation
 set CKPT_G=./output/%GAN_NAME%/checkpoints/RC49-ADCGAN-train-2024_01_05_06_39_53/model=G-current-weights-step=20000.pth
 set CKPT_G_EMA=./output/%GAN_NAME%/checkpoints/RC49-ADCGAN-train-2024_01_05_06_39_53/model=G_ema-current-weights-step=20000.pth
 set dump_niqe_path=D:/LocalWD/CcGAN_TPAMI_NIQE/RC-49/NIQE_64x64/fake_data
-
 python main.py -metrics none ^
     --data_dir %DATA_PATH% --cfg_file %CONFIG_PATH% --save_dir ./output/%GAN_NAME% ^
     --seed 2023 --num_workers 8 ^
